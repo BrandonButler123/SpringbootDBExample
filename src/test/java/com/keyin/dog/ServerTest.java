f5db4c8d8cd1d857ce542c9d3f8674a3cec7514b
Jordan <jordan.kelloway@keyin.com>
2024-07-03 11:59:37 -0230

    Added simple unit test

---
+++ src/test/java/com/keyin/dog/ServerTest.java
@@ -0,0 +1,12 @@
+package com.keyin.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ServerTest {
+    @Test
+    public void contextLoads() {
+
+    }
+}
